aacc04bf1a0a4a97901104f6e4dcf7405e6953a0
Box Chang <forkome@gmail.com>
2024-07-15 13:42:00 +0700

    Update

--- src/VNEDC/mes/urls.py
+++ src/VNEDC/mes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import re_path as url
-from .views import work_order_list, index
+from .views import work_order_list, index, runcard_detail, ipqc_log
 
 urlpatterns = [
-    url('work_orders/', work_order_list, name='work_order_list'),
+    url(r'^work_orders/', work_order_list, name='work_order_list'),
+    url(r'^detail/', runcard_detail, name='runcard_detail'),
+    url(r'^ipqc_log/', ipqc_log, name='runcard_ipqc_log'),
     url('', index, name='index'),
 ]
--- src/VNEDC/mes/views.py
+++ src/VNEDC/mes/views.py
@@ -16,9 +16,9 @@ def work_order_list(request):
     filters.append("InspectionDate BETWEEN %s AND %s")
     params.extend([thirty_days_ago.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d')])
 
-    if 'RunCardId' in request.GET and request.GET['RunCardId']:
-        filters.append("RunCardId LIKE %s")
-        params.append(f"%{request.GET['RunCardId']}%")
+    if 'Id' in request.GET and request.GET['Id']:
+        filters.append("r.Id LIKE %s")
+        params.append(f"%{request.GET['Id']}%")
 
     if 'InspectionDate' in request.GET and request.GET['InspectionDate']:
         filters.append("InspectionDate LIKE %s")
@@ -45,67 +45,76 @@ def work_order_list(request):
         params.append(f"%{request.GET['LineName']}%")
 
     sql = """
-    WITH IPQC AS (
-SELECT InspectionDate,r.WorkOrderId, Period,RunCardId, WorkCenterTypeName, MachineName,LineName,
-	MAX(CASE WHEN p.OptionName = 'Roll' THEN p.InspectionValue END) AS Roll,
-    MAX(CASE WHEN p.OptionName = 'Cuff' THEN p.InspectionValue END) AS Cuff,
-    MAX(CASE WHEN p.OptionName = 'Palm' THEN p.InspectionValue END) AS Palm,
-	MAX(CASE WHEN p.OptionName = 'Finger' THEN p.InspectionValue END) AS Finger,
-	MAX(CASE WHEN p.OptionName = 'FingerTip' THEN p.InspectionValue END) AS FingerTip,
-	MAX(CASE WHEN p.OptionName = 'Tensile' THEN p.InspectionValue END) AS Tensile,
-	MAX(CASE WHEN p.OptionName = 'Elongation' THEN p.InspectionValue END) AS Elongation
-  FROM [PMGMES].[dbo].[PMG_MES_RunCard] r
-  LEFT JOIN 
-    [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord] p ON r.Id = p.RunCardId
-  GROUP BY 
-    InspectionDate,r.WorkOrderId, Period,RunCardId, WorkCenterTypeName, MachineName,LineName COLLATE Chinese_Taiwan_Stroke_CI_AS
-),
-MeasureData AS (
-  SELECT 
-        LOT_NUMBER,
-        DATA_ID,
-        DATA_VAL,
-        ROW_NUMBER() OVER (PARTITION BY LOT_NUMBER COLLATE Chinese_Taiwan_Stroke_CI_AS ORDER BY DATA_ID DESC ) AS rn
-    FROM 
-        TGM.dbo.MEASURE_DATA
-)
-SELECT 
-InspectionDate,WorkOrderId, Period,RunCardId, WorkCenterTypeName, MachineName,LineName,
-    ipqc.RunCardId,
-    ipqc.Roll,
-	MAX(CASE WHEN md.rn = 5 THEN md.DATA_VAL END) AS measure_5,
-    ipqc.Cuff,
-	MAX(CASE WHEN md.rn = 4 THEN md.DATA_VAL END) AS measure_4,
-	ipqc.Palm,
-	MAX(CASE WHEN md.rn = 3 THEN md.DATA_VAL END) AS measure_3,
-    ipqc.Finger,
-	MAX(CASE WHEN md.rn = 2 THEN ROUND(TRY_CAST(md.DATA_VAL AS FLOAT) / 2, 2) END) AS measure_2,
-	ipqc.FingerTip,
-    MAX(CASE WHEN md.rn = 1 THEN ROUND(TRY_CAST(md.DATA_VAL AS FLOAT) / 2, 3) END) AS measure_1
-FROM 
-    IPQC ipqc
-LEFT OUTER JOIN 
-    MeasureData md ON ipqc.RunCardId COLLATE Chinese_Taiwan_Stroke_CI_AS = md.lot_number COLLATE Chinese_Taiwan_Stroke_CI_AS
-GROUP BY 
-InspectionDate,WorkOrderId, Period,RunCardId, WorkCenterTypeName, MachineName,LineName,
-    ipqc.RunCardId,
-    ipqc.Roll,
-    ipqc.Cuff,
-	ipqc.Palm,
-    ipqc.Finger,
-	ipqc.FingerTip 
+    select InspectionDate,r.WorkOrderId,Period,r.Id,WorkCenterTypeName,MachineName,LineName,ProductItem,CustomerCode,CustomerName,CustomerPartNo,AQL,w.PlanQty
+    from [PMGMES].[dbo].[PMG_MES_RunCard] r
+    LEFT JOIN [PMGMES].[dbo].[PMG_MES_WorkOrder] w on r.WorkOrderId = w.id
     """
     if filters:
-        sql += " HAVING " + " AND ".join(filters)
+        sql += " Where " + " AND ".join(filters)
 
     # 添加默认排序条件
-    sql += " ORDER BY ipqc.InspectionDate DESC, ipqc.WorkOrderId, TRY_CAST(ipqc.Period AS INT)"
+    sql += " ORDER BY InspectionDate DESC, WorkOrderId"
 
     db = mes_database()
     work_orders = db.select_sql_dict_param(sql, params)
 
     return JsonResponse(work_orders, safe=False)
 
+
+def runcard_detail(request):
+    runcard = request.GET.get('runcard')
+    sql = f"""
+    select r.id,r.WorkOrderId,w.CustomerName,w.PartNo,w.ProductItem,q.OptionName,q.Upper_InspectionValue,
+    q.InspectionValue,q.Lower_InspectionValue,q.InspectionStatus, q.InspectionUnit,q.DefectCode, FORMAT(q.CreationTime, 'yyyy/MM/dd HH:mm:ss') as CreationTime
+    from [PMGMES].[dbo].[PMG_MES_RunCard] r
+    LEFT JOIN [PMGMES].[dbo].[PMG_MES_WorkOrder] w on r.WorkOrderId = w.id
+    LEFT JOIN [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord] q on q.RunCardId = r.id
+    where r.id = '{runcard}'
+    """
+    db = mes_database()
+    results = db.select_sql_dict(sql)
+
+    sql2 = f"""
+    SELECT 
+        LOT_NUMBER,
+        DATA_ID,
+        DATA_VAL,
+        ROW_NUMBER() OVER (PARTITION BY LOT_NUMBER ORDER BY DATA_ID DESC ) AS rn
+    FROM 
+        TGM.dbo.MEASURE_DATA where LOT_NUMBER = '{runcard}'
+    """
+    measures = db.select_sql_dict(sql2)
+
+    if measures:
+        for result in results:
+            if result['OptionName'] == "Roll":
+                result['InspectionValue'] = str(result['InspectionValue']) + " (" + str(round(float(measures[4]['DATA_VAL']), 2))+ ")"
+            elif result['OptionName'] == "Cuff":
+                result['InspectionValue'] = str(result['InspectionValue']) + " (" + str(round(float(measures[3]['DATA_VAL']), 2)) + ")"
+            elif result['OptionName'] == "Palm":
+                result['InspectionValue'] = str(result['InspectionValue']) + " (" + str(round(float(measures[2]['DATA_VAL']), 2)) + ")"
+            elif result['OptionName'] == "Finger":
+                result['InspectionValue'] = str(result['InspectionValue']) + " (" + str(round(float(measures[1]['DATA_VAL'])/2, 3)) + ")"
+            elif result['OptionName'] == "FingerTip":
+                result['InspectionValue'] = str(result['InspectionValue']) + " (" + str(round(float(measures[0]['DATA_VAL'])/2, 3)) + ")"
+
+    return JsonResponse(results, safe=False)
+
+
+def ipqc_log(request):
+    runcard = request.GET.get('runcard')
+    OptionName = request.GET.get('OptionName')
+
+    sql = f"""
+    SELECT RunCardId,OptionName,InspectionValue,DefectCode,InspectionStatus,
+    LogAction,FORMAT(CreationTime, 'yyyy/MM/dd HH:mm:ss') as CreationTime
+    FROM [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord_Log] 
+    where RunCardId = '{runcard}' and OptionName = '{OptionName}'"""
+
+    db = mes_database()
+    results = db.select_sql_dict(sql)
+
+    return JsonResponse(results, safe=False)
 
 def index(request):
     return render(request, 'mes/index.html')
