aeb05122ee2a4e2df271159fc1ca19a7998d1385
Box Chang <forkome@gmail.com>
2024-07-26 08:52:25 +0700

    Update

--- src/VNEDC/jobs/scada2edc.py
+++ src/VNEDC/jobs/scada2edc.py
@@ -1,3 +1,8 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from jobs.database import vnedc_database, scada_database
 from datetime import datetime, timedelta
 
