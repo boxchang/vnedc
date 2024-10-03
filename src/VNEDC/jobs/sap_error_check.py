ed4fa203c5578b3584973643f5f7f904cb17b9d6
Box Chang <forkome@gmail.com>
2024-10-03 09:52:01 +0700

    Update

--- src/VNEDC/jobs/sap_error_check.py
+++ src/VNEDC/jobs/sap_error_check.py
@@ -1,6 +1,11 @@
 from jobs.database import mes_database
 import requests
 import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 def send_message(msg):
     path = os.path.dirname(os.path.abspath(__file__))
     wecom_file = os.path.join(path, "wecom_key.config")
