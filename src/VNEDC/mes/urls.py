09f19d83ede5a2ff3f8d2e50bf93266a69f2f15b
Le Long Cuong <108629737+llcuong@users.noreply.github.com>
2024-10-24 10:20:37 +0700

    UPDATE

--- src/VNEDC/mes/urls.py
+++ src/VNEDC/mes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path as url
 from spiderweb.views import spiderweb
 from mes.views import work_order_list, index, runcard_detail, ipqc_log, fast_check, runcard_info, \
-    runcard_api, thickness_data_api, fast_check2, machine_status
+    runcard_api, thickness_data_api, fast_check2
 
 urlpatterns = [
     url(r'^work_orders/', work_order_list, name='work_order_list'),
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^runcard_info/', runcard_info, name='runcard_info'),
     url(r'^fast_check/', fast_check, name='fast_check'),
     url(r'^fast_check2/', fast_check2, name='fast_check2'),
-    url(r'^machine_status/', machine_status, name='machine_status'),
     url(r'^runcard_api/(?P<runcard>[\w-]+)/$', runcard_api, name='runcard_api'),
     url(r'^thickness_data_api/', thickness_data_api, name='thickness_data_api'),
     url(r'^spiderweb/', spiderweb, name='spiderweb'),
