607fa013487d3957b0ecbdff21db0c35b3e86143
Box Chang <forkome@gmail.com>
2025-01-14 11:26:22 +0700

    Update

--- src/VNEDC/mes/views.py
+++ src/VNEDC/mes/views.py
@@ -959,7 +959,7 @@ def insert_parameter(request):
                 sql = f"""
                     INSERT INTO [VNEDC].[dbo].[collection_parametervalue]
                     (data_date, plant_id, mach_id, process_type, data_time, parameter_name, parameter_value, create_at, update_at, create_by_id, update_by_id)
-                    VALUES ('{data_date}', '{plant_id}', '{mach_id}', '{process_type}', '{data_time}', '{parameter_name}', {parameter_value}, Cast('{create_at}' as datetime2), Cast('{create_at}' as datetime2), Cast('{create_id}' as int), Cast('{create_id}' as int))
+                    VALUES ('{data_date}', '{plant_id}', '{mach_id}', '{process_type}', '{data_time}', '{parameter_name}', {parameter_value}, Cast('{create_at}' as datetime2), GETDATE(), Cast('{create_id}' as int), Cast('{create_id}' as int))
                 """
                 db.execute_sql(sql)
                 status = True
