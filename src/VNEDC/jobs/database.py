819405d86f3eff4614f786d4928fa6483e1d5fd5
Box Chang <forkome@gmail.com>
2025-03-12 09:14:45 +0700

    Update

--- src/VNEDC/jobs/database.py
+++ src/VNEDC/jobs/database.py
@@ -190,7 +190,7 @@ class tgm_gdpvc_database:
 
         return None
 
-class mes_database:
+class gd_mes_database:
     def select_sql(self, sql):
         self.conn = self.create_mes_connection()
         self.cur = self.conn.cursor()
@@ -218,6 +218,44 @@ class mes_database:
         try:
             conn = pyodbc.connect("DRIVER={{SQL Server}};SERVER={server}; database={database}; \
                                    trusted_connection=no;UID={uid};PWD={pwd}".format(server="10.13.102.22",
+                                                                                     database="PMGMES",
+                                                                                     uid="scadauser",
+                                                                                     pwd="pmgscada+123"))
+            return conn
+        except Error as e:
+            print(e)
+
+        return None
+
+
+class lk_mes_database:
+    def select_sql(self, sql):
+        self.conn = self.create_mes_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+        return self.cur.fetchall()
+
+    def select_sql_dict(self, sql):
+        self.conn = self.create_mes_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+
+        desc = self.cur.description
+        column_names = [col[0] for col in desc]
+        data = [dict(zip(column_names, row))
+                for row in self.cur.fetchall()]
+        return data
+
+    def execute_sql(self, sql):
+        self.conn = self.create_mes_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+        self.conn.commit()
+
+    def create_mes_connection(self):
+        try:
+            conn = pyodbc.connect("DRIVER={{SQL Server}};SERVER={server}; database={database}; \
+                                   trusted_connection=no;UID={uid};PWD={pwd}".format(server="10.14.102.11",
                                                                                      database="PMGMES",
                                                                                      uid="scadauser",
                                                                                      pwd="pmgscada+123"))
--- src/VNEDC/jobs/mes_product2edc.py
+++ src/VNEDC/jobs/mes_product2edc.py
@@ -4,14 +4,24 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
-from jobs.database import mes_database, vnedc_database
+from jobs.database import vnedc_database, lk_mes_database, gd_mes_database
 
 
-class mes_production_info(object):
-    today = ""
+class factory(object):
 
-    def __init__(self, today):
-        self.today = today
+    mes_db = None
+    vnedc_db = None
+    date = None
+    plant = None
+
+    def __init__(self, date, plant):
+        if plant == "GD":
+            self.mes_db = gd_mes_database()
+        elif plant == "LK":
+            self.mes_db = lk_mes_database()
+        self.vnedc_db = vnedc_database()
+        self.date = date
+        self.plant = plant
 
     def extract_substring(self, input_string):
         # 找到第一个 '-' 的位置
@@ -32,6 +42,8 @@ class mes_production_info(object):
                 dash_substring = input_string[first_dash_index + 1:second_dash_index]
 
             substring = dash_substring.split(" ")[0]
+
+        substring = substring if len(str(substring).strip()) > 0 else dash_substring
 
         # 检查子字符串是否以 'XL' 结尾
         if substring.endswith('XXL') or substring.endswith('XXS'):
@@ -64,11 +76,7 @@ class mes_production_info(object):
             return result
 
 
-    def main(self):
-        today = self.today
-
-        db = mes_database()
-        vnedc_db = vnedc_database()
+    def insert_daily_prod_info_head(self):
 
         sql = f"""
         SELECT distinct InspectionDate, Name, ProductItem, LineName
@@ -76,16 +84,18 @@ class mes_production_info(object):
           join [PMGMES].[dbo].[PMG_DML_DataModelList] dl on w.MachineId = dl.Id
           join [PMGMES].[dbo].[PMG_MES_RunCard] r on r.WorkOrderId = w.id
           join [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord] ipqc on ipqc.RunCardId = r.Id
-          where InspectionDate = '{today}' and Period between 6 and 23
+          where InspectionDate = '{self.date}' and Period between 6 and 23
           and Name like '%NBR%'
           and OptionName = 'Weight'
-		  and InspectionValue > 0
+          and InspectionValue > 0
           order by Name, LineName
         """
 
-        results = db.select_sql_dict(sql)
+        results = self.mes_db.select_sql_dict(sql)
+
 
         for result in results:
+            print(result["ProductItem"])
             size = self.extract_substring(result["ProductItem"])
             machine = str(result["Name"]).replace('_', '')
             mach_id = machine[2:7] + machine[-2:]
@@ -98,72 +108,88 @@ class mes_production_info(object):
             select * from [VNEDC].[dbo].[collection_daily_prod_info_head] where data_date = '{data_date}'
             and line = '{line}' and product = '{product}' and mach_id = '{mach_id}' and plant_id = '{plant_id}'
             """
-            rows = vnedc_db.select_sql_dict(sql)
+            rows = self.vnedc_db.select_sql_dict(sql)
 
             if not rows:
                 sql = f"""
                 insert into [VNEDC].[dbo].[collection_daily_prod_info_head](data_date, plant_id, mach_id, line, product, size, create_at, create_by_id)
                 values('{data_date}', '{plant_id}', '{mach_id}', '{line}', '{product}', '{size}', GETDATE(), 1)
                 """
-                vnedc_db.execute_sql(sql)
+                self.vnedc_db.execute_sql(sql)
 
-def insert_weight(selected_date):
-    sql = f"""
-        SELECT rc.WorkCenterTypeName, rc.MachineName, rc.Id, rc.InspectionDate, rc.Period,  rc.LineName, ir.InspectionValue
-        FROM [PMGMES].[dbo].[PMG_MES_RunCard] rc
-        JOIN [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord] ir
-        ON rc.Id = ir.RunCardId
-        AND ir.OptionName = 'Weight'
-        WHERE rc.WorkCenterTypeName = 'NBR'
-        AND (Period = 0 OR Period = 6 OR Period = 12 or Period = 18)
-        AND rc.InspectionDate = '{selected_date}'
-        ORDER BY Cast(Period as INT), MachineName, LineName
-    """
-    db = mes_database()
-    vnedc_db = vnedc_database()
-    rows = db.select_sql_dict(sql)
-    for row in rows:
-        count = 0
-        sdata_date = row['InspectionDate']
-
-        splant_id = ""
-        if 'NBR' == str(row['WorkCenterTypeName']):
-            splant_id = "GDNBR"
-
-        smach_id = ""
-        if splant_id == "GDNBR":
-            smach_id = "GDNBR" + str(row['MachineName'])[-2:]
-
-        if str(row['Period']) == '0':
-            sdata_time = '00'
-        elif str(row['Period']) == '6':
-            sdata_time = '06'
-        elif str(row['Period']) == '12':
-            sdata_time = '12'
-        else:
-            sdata_time = '18'
-        sparameter_value = float(row['InspectionValue'])
-
-        check_sql = f"""
-                select * from [VNEDC].[dbo].[collection_parametervalue] 
-                where data_date = '{sdata_date}' and plant_id = '{splant_id}' and mach_id = '{smach_id}' and process_type = 'OTHER' and data_time = {sdata_time} and parameter_name = 'WEIGHT'
+    def insert_weight(self):
+        sql = f"""
+            SELECT rc.WorkCenterTypeName, rc.MachineName, rc.Id, rc.InspectionDate, rc.Period,  rc.LineName, ir.InspectionValue
+            FROM [PMGMES].[dbo].[PMG_MES_RunCard] rc
+            JOIN [PMGMES].[dbo].[PMG_MES_IPQCInspectingRecord] ir
+            ON rc.Id = ir.RunCardId
+            AND ir.OptionName = 'Weight'
+            WHERE rc.WorkCenterTypeName = 'NBR'
+            AND (Period = 0 OR Period = 6 OR Period = 12 or Period = 18)
+            AND rc.InspectionDate = '{self.date}'
+            ORDER BY Cast(Period as INT), MachineName, LineName
         """
-        check = vnedc_db.select_sql_dict(check_sql)
-        if check:
-            continue
-        elif not check:
-            insert_sql = f"""
-                insert into [VNEDC].[dbo].[collection_parametervalue] (data_date, plant_id, mach_id, process_type, data_time, parameter_name, parameter_value, create_at, update_at, create_by_id, update_by_id)
-                values ('{sdata_date}', '{splant_id}', '{smach_id}', 'OTHER', '{sdata_time}', 'WEIGHT', {sparameter_value}, GETDATE(), GETDATE(), 30, 30)
+
+        rows = self.mes_db.select_sql_dict(sql)
+        for row in rows:
+
+            sdata_date = row['InspectionDate']
+
+            splant_id = ""
+            if 'NBR' == str(row['WorkCenterTypeName']):
+                splant_id = f"{self.plant}NBR"
+
+            smach_id = f"{self.plant}NBR" + str(row['MachineName'])[-2:]
+
+            if str(row['Period']) == '0':
+                sdata_time = '00'
+            elif str(row['Period']) == '6':
+                sdata_time = '06'
+            elif str(row['Period']) == '12':
+                sdata_time = '12'
+            else:
+                sdata_time = '18'
+            sparameter_value = float(row['InspectionValue'])
+
+            check_sql = f"""
+                    select * from [VNEDC].[dbo].[collection_parametervalue] 
+                    where data_date = '{sdata_date}' and plant_id = '{splant_id}' and mach_id = '{smach_id}' and process_type = 'OTHER' and data_time = {sdata_time} and parameter_name = 'WEIGHT'
             """
-            vnedc_db.execute_sql(insert_sql)
+            check = self.vnedc_db.select_sql_dict(check_sql)
+            if check:
+                continue
+            elif not check:
+                insert_sql = f"""
+                    insert into [VNEDC].[dbo].[collection_parametervalue] (data_date, plant_id, mach_id, process_type, data_time, parameter_name, parameter_value, create_at, update_at, create_by_id, update_by_id)
+                    values ('{sdata_date}', '{splant_id}', '{smach_id}', 'OTHER', '{sdata_time}', 'WEIGHT', {sparameter_value}, GETDATE(), GETDATE(), 30, 30)
+                """
+                self.vnedc_db.execute_sql(insert_sql)
+
+
+class MES2EDC(object):
+    plant_list = None
+    date = None
+
+    def __init__(self, date, plant_list):
+        self.date = date
+        self.plant_list = plant_list
+
+    def main(self):
+        for plant in self.plant_list:
+            f = factory(self.date, plant)
+            f.insert_daily_prod_info_head()
+            f.insert_weight()
+
+
+
 
 
 from datetime import datetime, timedelta
 
 today = datetime.today()
 today = today.strftime('%Y-%m-%d')
-insert_weight(today)
 
-prod_info = mes_production_info(today)
-prod_info.main()
+plant_list = ['LK', 'GD']
+
+m = MES2EDC(today, plant_list)
+m.main()
