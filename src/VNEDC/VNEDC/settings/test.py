ab9ff80f4e2a2c9e5a6893e6e0e4db3ee062e9a4
Box Chang <forkome@gmail.com>
2025-03-12 10:56:48 +0700

    Update

--- src/VNEDC/VNEDC/settings/test.py
+++ src/VNEDC/VNEDC/settings/test.py
@@ -46,12 +46,24 @@ DATABASES = {
             'driver': 'ODBC Driver 17 for SQL Server',
         },
     },
-    'MES': {
+    'GDMES': {
         'ENGINE': 'mssql',
         'NAME': 'PMGMES',
         'USER': 'scadauser',
         'PASSWORD': 'pmgscada+123',
         'HOST': '10.13.102.22',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
+    'LKMES': {
+        'ENGINE': 'mssql',
+        'NAME': 'PMGMES',
+        'USER': 'scadauser',
+        'PASSWORD': 'pmgscada+123',
+        'HOST': '10.14.102.11',
         'PORT': '1433',
 
         'OPTIONS': {
