6f6bb08513887b84cfb6fb2ae35b98525b77ad9a
Box Chang <forkome@gmail.com>
2025-03-17 21:29:53 +0700

    Update

--- src/VNEDC/VNEDC/settings/test.py
+++ src/VNEDC/VNEDC/settings/test.py
@@ -22,11 +22,11 @@ DATABASES = {
             'driver': 'ODBC Driver 17 for SQL Server',
         },
     },
-    'VNEDC': {
+    'SGADA': {
         'ENGINE': 'mssql',
-        'NAME': 'VNEDC_Test',
-        'USER': 'vnedc_test',
-        'PASSWORD': 'vnedc_test#2024',
+        'NAME': 'PMG_DEVICE',
+        'USER': 'scadauser',
+        'PASSWORD': 'pmgscada+123',
         'HOST': '10.13.104.181',
         'PORT': '1433',
 
@@ -34,76 +34,52 @@ DATABASES = {
             'driver': 'ODBC Driver 17 for SQL Server',
         },
     },
-    # 'default': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'VNEDC',
-    #     'USER': 'vnedc',
-    #     'PASSWORD': 'vnedc#2024',
-    #     'HOST': '10.13.104.181',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-    # 'SGADA': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'PMG_DEVICE',
-    #     'USER': 'scadauser',
-    #     'PASSWORD': 'pmgscada+123',
-    #     'HOST': '10.13.104.181',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-    # 'SAP': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'PMG_DEVICE',
-    #     'USER': 'sa',
-    #     'PASSWORD': '!QAw3ed',
-    #     'HOST': '10.13.102.22',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-    # 'GDMES': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'PMGMES',
-    #     'USER': 'scadauser',
-    #     'PASSWORD': 'pmgscada+123',
-    #     'HOST': '10.13.102.22',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-    # 'LKMES': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'PMGMES',
-    #     'USER': 'scadauser',
-    #     'PASSWORD': 'pmgscada+123',
-    #     'HOST': '10.14.102.11',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
-    # 'VNEDC': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'VNEDC',
-    #     'USER': 'vnedc',
-    #     'PASSWORD': 'vnedc#2024',
-    #     'HOST': '10.13.104.181',
-    #     'PORT': '1433',
-    #
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #     },
-    # },
+    'SAP': {
+        'ENGINE': 'mssql',
+        'NAME': 'PMG_DEVICE',
+        'USER': 'sa',
+        'PASSWORD': '!QAw3ed',
+        'HOST': '10.13.102.22',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
+    'GDMES': {
+        'ENGINE': 'mssql',
+        'NAME': 'PMGMES',
+        'USER': 'scadauser',
+        'PASSWORD': 'pmgscada+123',
+        'HOST': '10.13.102.22',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
+    'LKMES': {
+        'ENGINE': 'mssql',
+        'NAME': 'PMGMES',
+        'USER': 'scadauser',
+        'PASSWORD': 'pmgscada+123',
+        'HOST': '10.14.102.11',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
+    'VNEDC': {
+        'ENGINE': 'mssql',
+        'NAME': 'VNEDC',
+        'USER': 'vnedc',
+        'PASSWORD': 'vnedc#2024',
+        'HOST': '10.13.104.181',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
+    },
 }
